5e1258302b48a53504a4cb86eb21b705f2ad1adc
Muhammad Faisal <muh.faisal572@gmail.com>
2023-03-11 14:14:51 +0700

    add navigation bar[navbar] and responsif

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ import styles from "./style";
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className="{`${styles.paddingX} ${styles.flexCenter}`}">
-        <div className="{`${styles.boxWidth}`}">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
 
       <div className="bg-primary{`${styles.flexStart}`}">
-        <div className="{`${styles.boxWidth}`}">
+        <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,55 @@ import { useState } from "react";
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constant";
 
-const Navbar = () => (
-  <nav className="w-full flex py-6 justify-between items-center navbar">
-    <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
-    <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-      {navLinks.map((nav, index) => {
-        <li
-          key={nav.id}
-          className={`font-poppins font-normal cursor-pointer text-wihte`}
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      {/* nav list for website */}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }  text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      {/* nav list for mobile */}
+      <div className="sm:hidden flex fflex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <a href={`#${nav.id}`}>{nav.title}</a>
-        </li>;
-      })}
-    </ul>
-  </nav>
-);
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px]  ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }  text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
